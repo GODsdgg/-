d3f16ddf8ef3ee718a814899d898cb4a986b08b6
Godsdgg <m15130638277@163.com>
2018-11-07 21:02:07 +0800

    多方式登录(第二种方式)

--- mall/mall/settings.py
+++ mall/mall/settings.py
@@ -254,4 +254,5 @@ JWT_AUTH = {
 }
 
 # AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend']
-AUTHENTICATION_BACKENDS = ['utils.users.MobileUsernameModelBackend']
+# AUTHENTICATION_BACKENDS = ['utils.users.MobileUsernameModelBackend']
+AUTHENTICATION_BACKENDS = ['utils.users.SettingsBackend']
