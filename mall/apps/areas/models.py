df2846e76a178879c440286b1bed2b5985aaa87c
Godsdgg <m15130638277@163.com>
2018-11-11 15:04:42 +0800

    省市区模型

--- mall/apps/areas/models.py
+++ mall/apps/areas/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Area(models.Model):
+    """
+    行政区划
+    """
+    name = models.CharField(max_length=20, verbose_name='名称')
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, related_name='subs', null=True, blank=True, verbose_name='上级行政区划')
+
+    class Meta:
+        db_table = 'tb_areas'
+        verbose_name = '行政区划'
+        verbose_name_plural = '行政区划'
+
+    def __str__(self):
+        return self.name
+
+
+# id           name        parent_id
+
+# 10000         河北省         NULL
+
+# 10200         保定市             10000
+
+# 10210         定兴县          10200
+# 10220         涿州市          10200
--- mall/mall/settings.py
+++ mall/mall/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'oauth.apps.OauthConfig',
+    'areas.apps.AreasConfig',
 ]
 
 MIDDLEWARE = [
