2bdcc974b66f8d357dcbdc764b8972c3304622dd
Godsdgg <m15130638277@163.com>
2018-11-13 20:36:06 +0800

    自定义文件存储系统

---
+++ mall/utils/fastdfs/__init__.py
@@ -0,0 +1,8 @@
+"""
+{'Local file name': '/home/python/Desktop/images/010.jpg',
+'Uploaded size': '337.00KB', 'Storage IP': '192.168.177.128',
+'Remote file_id': 'group1/M00/00/00/wKixgFvqtwmARjtuAAVFpYSD7dI103.jpg',
+'Status': 'Upload successed.',
+'Group name': 'group1'}
+
+"""
